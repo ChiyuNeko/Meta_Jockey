afb4d69ec0e7f64f1422166ce9d1e1c81a6647c2
ChiyuNeko <joe26121@gmail.com>
2025-11-23 23:41:19 +0800

    11/23 update

--- Assets/Script/BubbleCounter.cs
+++ Assets/Script/BubbleCounter.cs
@@ -1,26 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class BubbleCounter : MonoBehaviour
 {
-    public int BubbleCount = 0;
+    public int BubbleCount;
+    public GameObject Sun;
     public GameObject TravleButton;
     public GameObject travelVFXPos;
     public GameObject travelVFX;
+    public GameObject Circle1;
+    public GameObject Circle2;
+    public Material Wall;
+    public Material RingLight1;
+    public Material RingLight2;
+    [ColorUsage(true, true)] 
+    public Color RingLightColor1;
+    [ColorUsage(true, true)] 
+    public Color RingLightColor2;
+    public float intnsitive1 = 0;
+    public float intnsitive2 = 0;
+    public float WallDissolve = 0;
     public static BubbleCounter bubbleCounter = new BubbleCounter();
     void Start()
     {
         bubbleCounter = this;
+        BubbleCount = 0;
+        Wall.SetFloat("_Dissolve", 80);
+        WallDissolve = 80;
+        RingLight1.SetColor("_EmissionColor", RingLightColor1 * 0);
+        RingLight2.SetColor("_EmissionColor", RingLightColor2 * 0);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (BubbleCount >= 20)
+        BubbleCount = Mathf.Clamp(BubbleCount, 0, 40);
+        Sun.transform.localScale = Vector3.Lerp(Sun.transform.localScale, Vector3.one * BubbleCount, 0.1f);
+        //Sun.transform.localScale = Vector3.one * BubbleCount;
+         if (BubbleCount >= 10)
+        {
+            intnsitive1 = Mathf.Lerp(intnsitive1, 2, Time.deltaTime/2);
+            Circle1.transform.Rotate(0,  0, Time.deltaTime * 50);
+            RingLight1.SetColor("_EmissionColor", RingLightColor1 * intnsitive1);
+        }
+         if (BubbleCount >= 20)
+        {
+            intnsitive2 = Mathf.Lerp(intnsitive1, 4, Time.deltaTime/2);
+            Circle2.transform.Rotate(0, 0, -Time.deltaTime * 50);
+            RingLight2.SetColor("_EmissionColor", RingLightColor2 * intnsitive2);
+        }
+        if (BubbleCount >= 30)
+        {
+            WallDissolve = Mathf.Lerp(WallDissolve, 0, Time.deltaTime/3);
+            Wall.SetFloat("_Dissolve", WallDissolve);
+        }
+        if (BubbleCount == 39)
         {
             TravleButton.SetActive(true);
-            BubbleCount = 0;
         }
     }
 
--- Assets/Script/HotKey.cs
+++ Assets/Script/HotKey.cs
@@ -31,15 +31,9 @@ public class HotKey : MonoBehaviour
     public float EQ;
     public float Vol;
     public ArduinoData arduinoData;
+    public int inputindex;
     public ParticleSystem[] particleSystems;
     public VisualEffect visualEffect;
-    bool flag1 = false;
-    bool flag2 = false;
-    bool flag3 = false;
-    bool flag4 = false;
-    bool flag5 = false;
-    bool flag6 = false;
-    bool flag7 = false;
     [Header("Controller Parameters")]
     public List<GameObject> Buttons = new List<GameObject>();
     public Material OriginalColor;
@@ -49,56 +43,57 @@ public class HotKey : MonoBehaviour
     public GameObject[] BigCircle;
     public float BigCircle1Rot;
     public float BigCircle2Rot;
+    public bool triggered = false;
     void Start()
     {
-        ArduinoBasic arduinoBasic = new ArduinoBasic();
         for (var i = 0; i < NoteNames.Length; i++) SetUpNoteAction(i);
         _modWheelAction.Enable();
+        inputindex = -1;
     }
 
     // Update is called once per frame
     void Update()
     {
         var mod = _modWheelAction.ReadValue<float>();
-         Debug.Log("" + mod);
-        switch (LoopCount)
-        {
-            case 0:
-                if (BigCircle[0].activeSelf)
-                {
-                    foreach (GameObject i in BigCircle)
-                    {
-                        i.SetActive(false);
-                    }
-                }
-                break;
-            case 1:
-                if(!BigCircle[0].activeSelf)
-                    BigCircle[0].SetActive(true);
-                if(BigCircle[1].activeSelf)
-                    BigCircle[1].SetActive(false);
-                break;
+        Debug.Log("" + mod);
+        // switch (LoopCount)
+        // {
+        //     case 0:
+        //         if (BigCircle[0].activeSelf)
+        //         {
+        //             foreach (GameObject i in BigCircle)
+        //             {
+        //                 i.SetActive(false);
+        //             }
+        //         }
+        //         break;
+        //     case 1:
+        //         if(!BigCircle[0].activeSelf)
+        //             BigCircle[0].SetActive(true);
+        //         if(BigCircle[1].activeSelf)
+        //             BigCircle[1].SetActive(false);
+        //         break;
 
-            case 2:
-                if(!BigCircle[1].activeSelf)
-                    BigCircle[1].SetActive(true);
-                if(BigCircle[2].activeSelf)
-                    BigCircle[2].SetActive(false);
-                break;
+        //     case 2:
+        //         if(!BigCircle[1].activeSelf)
+        //             BigCircle[1].SetActive(true);
+        //         if(BigCircle[2].activeSelf)
+        //             BigCircle[2].SetActive(false);
+        //         break;
 
-            case 3:
-                if(!BigCircle[2].activeSelf)
-                    BigCircle[2].SetActive(true);
-                break;
+        //     case 3:
+        //         if(!BigCircle[2].activeSelf)
+        //             BigCircle[2].SetActive(true);
+        //         break;
 
-            default:
-                break;
-        }
+        //     default:
+        //         break;
+        // }
         BigCircle1Rot = Mathf.Lerp(BigCircle1Rot, arduinoData.encoder, 0.01f);
         BigCircle[0].transform.rotation = quaternion.Euler(0, 90, BigCircle1Rot);
         BigCircle[1].transform.Rotate(0, 0, (arduinoData.encoder2 + 20) * Time.deltaTime * 3);
 
-        EQ = Mathf.Lerp(EQ, arduinoData.encoder * 500 + 10000, 0.1f) ;
+        EQ = Mathf.Lerp(EQ, mod * 10000, 0.1f) ;
         EQ = Mathf.Clamp(EQ, 100, 10000);
         audioMixer.SetFloat("BaseLowPass", EQ);
         
@@ -106,122 +101,70 @@ public class HotKey : MonoBehaviour
         Vol = Mathf.Clamp(Vol, -80, 10);
         audioMixer.SetFloat("Vol", Vol);
         
-        if (Input.GetKeyDown(KeyCode.Keypad1) || (arduinoData.button1 == 0 && flag1 == false))
+        if (Input.GetKeyDown(KeyCode.Keypad1) || inputindex == 0)
         {
             Buttons[0].GetComponent<Renderer>().material = TriggerColor;
             audioSource[0].Play();
             StartCoroutine(ButtonRecover(Buttons[0], RecoverTime));
             visualEffect.Play();
-            flag1 = true;
-        }
-        if (arduinoData.button1 == 1)
-        {
-            flag1 = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.Keypad2) || (arduinoData.button2 == 0 && flag2 == false))
+        if (Input.GetKeyDown(KeyCode.Keypad2) || inputindex == 1)
         {
             Buttons[1].GetComponent<Renderer>().material = TriggerColor;
             audioSource[1].Play();
             StartCoroutine(ButtonRecover(Buttons[1], RecoverTime));
             particleSystems[0].Play();
-            flag2 = true;
-        }
-        if (arduinoData.button2 == 1)
-        {
-            flag2 = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.Keypad3) || (arduinoData.button3 == 0 && flag3 == false))
+        if (Input.GetKeyDown(KeyCode.Keypad3) || inputindex == 2)
         {
             Buttons[2].GetComponent<Renderer>().material = TriggerColor;
             audioSource[2].Play();
             StartCoroutine(ButtonRecover(Buttons[2], RecoverTime));
             visualEffect.Play();
-            flag3 = true;
-        }
-        if (arduinoData.button3 == 1)
-        {
-            flag3 = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.Keypad4) || (arduinoData.button4 == 0 && flag4 == false))
+        if (Input.GetKeyDown(KeyCode.Keypad4) || inputindex == 3)
         {
             Buttons[3].GetComponent<Renderer>().material = TriggerColor;
             audioSource[3].Play();
             StartCoroutine(ButtonRecover(Buttons[3], RecoverTime));
             particleSystems[0].Play();
-            flag4 = true;
-        }
-        if (arduinoData.button4 == 1)
-        {
-            flag4 = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.Keypad5) || (arduinoData.button5 == 0 && flag5 == false))
+        if (Input.GetKeyDown(KeyCode.Keypad5) || inputindex == 4)
         {
             Buttons[4].GetComponent<Renderer>().material = TriggerColor;
-            if (!audioSource[4].gameObject.activeSelf)
+            if (!triggered)
             {
-                audioSource[4].gameObject.SetActive(true);
-                //audioSource[4].Play();
-                LoopCount++;
-            }
-            else
-            {
-                audioSource[4].gameObject.SetActive(false);
-                LoopCount--;
+                audioSource[4].gameObject.SetActive(!audioSource[4].gameObject.activeSelf);
+                triggered = true;
             }
             StartCoroutine(ButtonRecover(Buttons[4], RecoverTime));
-            flag5 = true;
-        }
-        if (arduinoData.button5 == 1)
-        {
-            flag5 = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.Keypad6) || (arduinoData.button6 == 0 && flag6 == false))
+        if (Input.GetKeyDown(KeyCode.Keypad6) || inputindex == 5)
         {
             Buttons[5].GetComponent<Renderer>().material = TriggerColor;
-            if (!audioSource[5].gameObject.activeSelf)
+            if (!triggered)
             {
-                audioSource[5].gameObject.SetActive(true);
-                //audioSource[5].Play();
-                LoopCount++;
+                audioSource[5].gameObject.SetActive(!audioSource[5].gameObject.activeSelf);
+                triggered = true;
             }
-            else
-            {
-                audioSource[5].gameObject.SetActive(false);
-                LoopCount--;
-            }
+           
             StartCoroutine(ButtonRecover(Buttons[5], RecoverTime));
-            flag6 = true;
-        }
-        if (arduinoData.button6== 1)
-        {
-            flag6 = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.Keypad7) || (arduinoData.button7 == 0 && flag7 == false))
+        if (Input.GetKeyDown(KeyCode.Keypad7) || inputindex == 6)
         {
             Buttons[6].GetComponent<Renderer>().material = TriggerColor;
-            if (!audioSource[6].gameObject.activeSelf)
+            if (!triggered)
             {
-                audioSource[6].gameObject.SetActive(true);
-                //audioSource[6].Play();
-                LoopCount++;
-            }
-            else
-            {
-                audioSource[6].gameObject.SetActive(false);
-                LoopCount--;
+                audioSource[6].gameObject.SetActive(!audioSource[6].gameObject.activeSelf);
+                triggered = true;
             }
             StartCoroutine(ButtonRecover(Buttons[6], RecoverTime));
-            flag7 = true;
-        }
-        if (arduinoData.button7 == 1)
-        {
-            flag7 = false;
         }
         
     }
@@ -260,10 +203,12 @@ public class HotKey : MonoBehaviour
     }
     void OnNotePerformed(InputAction.CallbackContext ctx, int index)
     {
-        Debug.Log("" + index + "");
+        Debug.Log("Input" + index + "");
+        inputindex = index;       
     }
     void OnNoteCanceled(InputAction.CallbackContext ctx, int index)
     {
-        
+        inputindex = -1;
+        triggered = false;
     }
 }
