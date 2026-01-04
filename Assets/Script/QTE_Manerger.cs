ff1d96d86781617b0f6a8495d081208052f20094
ChiyuNeko <joe26121@gmail.com>
2026-01-04 17:10:21 +0800

    1/4 QTE adjust

--- Assets/Script/QTE_Manerger.cs
+++ Assets/Script/QTE_Manerger.cs
@@ -4,30 +4,42 @@ using UnityEngine;
 
 public class QTE_Manerger : MonoBehaviour
 {
+    public HandsParameters handsParameters;
+    public float velocityThreshold = 1.0f;
     public AudioSource clap;
     public GameObject prefab;
+    public GameObject wirePrefab;
+    public float delayTime = 1f;
     public Vector3 generateZone;
 
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(handsParameters.RightControllerVelocity > velocityThreshold)
         {
             clap.Play();
             for(int i = 0; i < 10; i++)
             {
                 Vector3 prefabPos = new Vector3(Random.Range(0,generateZone.x),Random.Range(0,generateZone.y),Random.Range(0,generateZone.z));
-                Instantiate(prefab, prefabPos + gameObject.transform.position, Quaternion.identity);
+                if(prefab)
+                    Instantiate(prefab, prefabPos + gameObject.transform.position, Quaternion.identity);
             }
         }
     }
-    private void OnDrawGizmos()
+
+    IEnumerator delay(float time)
+    {
+        yield return new WaitForSeconds(time);
+        if(wirePrefab)
+            Instantiate(wirePrefab);
+    }       
+    private void OnDrawGizmosSelected()
     {
         // Set the color with custom alpha.
-        Gizmos.color = new Color(1f, 0f, 0f, 0.7f);
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
 
         // Draw the cube.
-        Gizmos.DrawCube(transform.position, new Vector3(10,10,10));
+        Gizmos.DrawCube(transform.position + generateZone / 2, generateZone);
 
 
     }
