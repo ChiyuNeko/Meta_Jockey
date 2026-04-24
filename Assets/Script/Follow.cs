56b381a040aa9ca936e508ae7f801259f73bde5d
Devourar <wchen3326@gmail.com>
2026-04-24 20:44:49 +0800

    0424 Update 4

--- Assets/Script/Follow.cs
+++ Assets/Script/Follow.cs
@@ -6,7 +6,7 @@ public class Follow : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
-    public float moveScale;
+    public Vector3 moveScale;
     public bool followRotation;
     
     void Start()
@@ -20,7 +20,9 @@ public class Follow : MonoBehaviour
     {
         if (target != null)
         {
-            transform.localPosition = target.localPosition * moveScale + offset;
+            transform.localPosition =new Vector3(target.localPosition.x * moveScale.x + offset.x,
+                                                target.localPosition.y * moveScale.y + offset.y, 
+                                                target.localPosition.z * moveScale.z + offset.z) ;
             if (followRotation)
                 transform.rotation = target.rotation;
         }
